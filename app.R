51d5e16a1623ac421dd2bdddbc2b99c6aeb208ef
Abigail Beasley <abigailbeasley98@gmail.com>
2025-04-17 20:24:50 -0700

    style edits and table

--- app.R
+++ app.R
@@ -19,6 +19,7 @@ library(sf)
 library(tigris)
 library(plotly)
 library(jsonlite)
+library(bslib)
 
 file.exists("OverweightPrevalenceData.csv")
 
@@ -61,6 +62,7 @@ food_disparity_county_level$geo_id <- paste0("0500000US", food_disparity_county_
 # adding geo ID for mapping
 food_disparity_county_level$geo_id <- paste0("0500000US", food_disparity_county_level$fips)
 
+
 base_marker <- list(
   line = list(width = 0)
 )
@@ -87,6 +89,13 @@ state_mortality <- diabetes_mortality %>%
   filter(location_level == 'State') %>%
   mutate(state_code = state.abb[match(location_name, state.name)])
 
+# Filter to state-level data and add state codes 
+county_mortality <- diabetes_mortality %>%
+  filter(location_level == 'County')
+
+# adding geo ID for mapping
+county_mortality$geo_id <- paste0("0500000US", county_mortality$fips)
+
 # Filtering on state level
 state_level <- data %>%
   filter(location_level == 'State')
@@ -97,21 +106,27 @@ state_level$state <- state_level$location_name
 # adding state code for plotly map
 state_level$state_code <- state.abb[match(state_level$location_name, state.name)]
 
+## style guidelines
+plot_background_color = "#101010"
+
 # # Define UI for application that draws a histogram
 ui <- navbarPage("Overweight Population Trends",
-                 
+                 theme = bs_theme(bg = "#101010", # background color
+                                  fg = "#FFF", # txt color
+                                  primary = "#E69F00", # accent used in buttons
+                                  secondary = "#0072B2", # for secondary buttons
+                                  success = "#009E73", # color for badges and alerts
+                                  base_font = font_google("Inter") # font color
+                                  ),
                  # page 1: overweight trends
                  tabPanel('Overweight Trends',
                           fluidPage(
-                            tags$head(
-                              tags$style(HTML("
-                                              .well {
-                                                background-color: #e0f7fa !important;  /* light blue */
-                                                border: none;
-                                                box-shadow: none;
-                                              }
-                                              "))
-                            ),
+#                            tags$head( # styling
+#                              tags$style(HTML(".well {
+#                                                background-color: #e0f7fa !important;  /* light blue */
+#                                                border: none;
+ #                                               box-shadow: none;
+#                                              }"))),
                             #Application title
                             titlePanel("Trends in Overweight Prevalence Among Younger U.S. Demographics"),
                             
@@ -146,7 +161,7 @@ ui <- navbarPage("Overweight Population Trends",
                               # map and histogram
                               mainPanel(
                                 tabsetPanel(
-                                  tabPanel("Map", plotlyOutput("mapPlot")),
+                                  tabPanel("Geographic View: US Map", plotlyOutput("mapPlot")),
                                   tabPanel("Histogram", plotlyOutput("histPlot")),
                                   tabPanel("Time Series", plotlyOutput("timeSeriesPlot"))
                                 )
@@ -168,18 +183,19 @@ ui <- navbarPage("Overweight Population Trends",
                                 selectInput("mort_race",
                                           label = "Select Race",
                                           choices = unique(state_mortality$race_name),
-                                          selected = 'Total')),
+                                          selected = 'Total'),
+                                selectInput("mort_geo",
+                                            label = "Select Geographic Level",
+                                            choices = c('State', 'County'),
+                                            selected = 'State')),
                               # Plots
-                              mainPanel(
-                                tabsetPanel(
-                                  tabPanel("Map", plotlyOutput("mapMortality"))
-                                  )
-                                ) # end main panel
+                              mainPanel(plotlyOutput("mapMortality")
+                                        ) # end main panel
                           ) # end side bar layout
           ) # end page
           ), # end tab panel
           # page 3: Food Scarcity
-          tabPanel('Food Scarcity',
+          tabPanel('Poverty, Food Scarcity and Economic Inequality',
                    fluidPage(
                      titlePanel('Measures of Poverty and Access to Food by Geographic Location'),
                      
@@ -199,10 +215,8 @@ ui <- navbarPage("Overweight Population Trends",
                                      choices = c('State', 'County'),
                                      selected = 'State')),
                        # Plots
-                       mainPanel(
-                         tabsetPanel(
-                           tabPanel("Map", plotlyOutput("mapDisparities"))
-                         )
+                       mainPanel(plotlyOutput("mapDisparities"),
+                                 plotlyOutput("top10disparities")
                        ) # end main panel
                      ) # end side bar layout
                    ) # end page
@@ -239,70 +253,177 @@ server <- function(input, output) {
             hoverinfo = "text",
             hoverlabel=list(bgcolor = "#000080")) %>%
             layout(title = paste(overweight_label, "by State"),
-                   geo = list(scope = "usa"))
+                   font = list(color = "#ffffff",size = 12, family = "Arial"),
+                   geo = list(scope = "usa",
+                              bgcolor = plot_background_color),
+                   plot_bgcolor = plot_background_color,
+                   paper_bgcolor = plot_background_color)
   })
   
   # Map of Diabetes Mortality
   output$mapMortality <- renderPlotly({
     
-    # Filter data for selected gender, year, and age group
-    mort_data <- state_mortality %>%
-      filter(age_name==input$mort_age & race_name==input$mort_race)
+    if(input$mort_geo == 'State') {
+      # Filter data for selected gender, year, and age group
+      mort_data_state <- state_mortality %>%
+        filter(age_name==input$mort_age & race_name==input$mort_race)
+      
+      # Plotting the chorepleth map
+      plot_ly(data = mort_data_state, z = ~val * 100000, # mortality count per 100k
+              locations = ~state_code,  # state abbreviations (e.g., "CA")
+              type = "choropleth", locationmode = "USA-states",
+              colorscale = "Reds",
+              colorbar = list(title = "Mortality Rate (per 100k)"),
+              marker = list(line = list(color = "black", width = 0.5)),
+              text = ~paste0(location_name, "<br> Mortality Rate (per 100k): ", 
+                             round(val * 100000, 2), ""),
+              hoverinfo = "text") %>%
+        layout(title = "Mortality Rates for Diabetes by State (per 100k)",
+               font = list(color = "#ffffff",size = 12, family = "Arial"),
+               plot_bgcolor = plot_background_color,
+               paper_bgcolor=plot_background_color,
+               geo = list(scope = "usa",
+                          bgcolor = plot_background_color))
+      
+    } else {
+      # Filter data for selected gender, year, and age group
+      mort_data_county <- county_mortality %>%
+        filter(age_name==input$mort_age & race_name==input$mort_race)
+      
+      # county level plot 
+      plot_ly(data = mort_data_county,
+              type = "choropleth",
+              geojson = geojson_data,
+              featureidkey = "properties.GEO_ID",
+              locations = ~geo_id,
+              z = ~val * 100000,
+              text = ~paste0(location_name, "<br> Mortality Rate (per 100k): ", 
+                             round(val * 100000, 2), ""),
+              hoverinfo = "text",
+              colorscale = "Reds",
+              colorbar = list(title = "Deaths per 100k"),
+              marker = list(line = list(width = 0))) %>%
+        layout(title = paste(label, 'by U.S. County'),
+               plot_bgcolor = plot_background_color,
+               paper_bgcolor=plot_background_color,
+               
+               geo = list(scope = "usa",
+                          bgcolor = plot_background_color))
+      
+    }
     
-    # Plotting the chorepleth map
-    plot_ly(data = mort_data, z = ~val * 100000, # mortality count per 100k
-            locations = ~state_code,  # state abbreviations (e.g., "CA")
-            type = "choropleth", locationmode = "USA-states",
-            colorscale = "Reds",
-            colorbar = list(title = "Mortality Rate (Per 100k)"),
-            marker = list(line = list(color = "black", width = 0.5)),
-            text = ~paste0(location_name, "<br> Mortality Rate (per 100k People): ", 
-                           round(val * 100000, 2), ""),
-            hoverinfo = "text") %>%
-      layout(title = "Prevalence of Overweight Populations by State",
-             geo = list(scope = "usa"))
   })
   
   # Map of Food Disparity
   output$mapDisparities <- renderPlotly({
     
-    # get label
+    # get pretty label for variable to use for title
     label <- label_dict[[input$variable]]
-    if (is.null(label)) label <- input$variable  # fallback
     
+    # If user wants to see data on state level
     if (input$geo == 'State') {
-      plot_ly(data = food_disparity_state_level,
-              z = food_disparity_state_level[[input$variable]],
-              locations = ~state_code, type = "choropleth",
-              marker = list(line = list(color = "black", width = 0.5)),
-              locationmode = "USA-states", colorscale = "Reds",
-              text = ~paste0(State,"<br>", label, ': ',
+      # state level plot
+      plot_ly(data = food_disparity_state_level, # data on date level
+              type = "choropleth", # type of plot--map with filled in values reflecting variable of interest
+              z = food_disparity_state_level[[input$variable]], # getting user-selected variable
+              locations = ~state_code, # abbreviations in data to map to US map
+              locationmode = "USA-states", # map is of the US
+              marker = list(line = list(color = "black", width = 0.5)), # changing line width and color
+              colorscale = "Reds", # adding scale for color fill
+              # custom hover info
+              text = ~paste0(State,"<br>", label, ': ', 
                              round(food_disparity_state_level[[input$variable]], 2), ""),
-              hoverinfo = "text") %>%
-        layout(title = paste(label, 'by U.S. State'),
-               geo = list(scope = "usa"))
-    } else { # county level
-      # Now build the plot
+              hoverinfo = "text", #  assigning text as custom hover text
+              colorbar=list(title=label)) %>%
+        layout(title = paste(label, 'by U.S. State'),  # Map title 
+               font = list(color = "#fff",size = 12, family = "Arial"), #font
+               plot_bgcolor = plot_background_color,
+               paper_bgcolor=plot_background_color,
+               geo = list(scope = "usa", # zoom on US
+                          bgcolor = plot_background_color))
+    } else { # If user asks for county level
+      # county level plot 
       plot_ly(data = food_disparity_county_level,
               type = "choropleth",
-              geojson = geojson_data,
-              featureidkey = "properties.GEO_ID",
-              locations = ~geo_id,
-              z = ~PovertyRate,
+              geojson = geojson_data, # JSON file with data to build counties
+              featureidkey = "properties.GEO_ID", # merge key in json file for data
+              locations = ~geo_id, # geo file in data to map to counties
+              z = food_disparity_county_level[[input$variable]], # variable of interest
+              # custom text for hover
               text = ~paste("Location:", County,
-                            "<br>", label, ":", round(PovertyRate, 1)),
+                            "<br>", label, ":",
+                            round(food_disparity_county_level[[input$variable]], 1)),
               hoverinfo = "text",
-              colorscale = "Reds",
-              colorbar = list(title = "Death Count"),
-              marker = list(line = list(width = 0))) %>%
-        layout(title = paste(label, 'by U.S. County'),
-               geo = list(scope = "usa",
-                          showlakes = TRUE,
-                          lakecolor = "white"))
+              colorscale = "Reds", #custom fill gradient
+              colorbar = list(title = label), # adding formatted variable name to legend
+              marker = list(line = list(width = 1))) %>% # custom lines (very small for counties)
+        layout(title = paste(label, 'by U.S. County'), # Map title 
+               font = list(color = "#fff",size = 12, family = "Arial"), # font
+               geo = list(scope = "usa", # zoom on US
+                          bgcolor = plot_background_color),
+               plot_bgcolor = plot_background_color,
+               paper_bgcolor=plot_background_color)
       
     }
-    
   })
+  
+  # adding a table of top 10
+  output$top10disparities <- renderPlotly({
+    
+    # get pretty label for variable to use for title
+    label <- label_dict[[input$variable]]
+    
+    
+    # converting input to symbol not string for filtering
+    var_sym <- sym(input$variable)
+    
+    if (input$geo == 'State') {
+      
+      # filtering and sorting table data
+      table_data = food_disparity_state_level %>%
+        arrange(desc(!!var_sym)) %>% 
+        mutate(disp_var = comma(!!var_sym)) %>% # adding commas to big nums
+        select(State, disp_var) %>%
+        head(10)
+      
+      # Create plotly table
+      plot_ly(type = 'table',
+              header = list(values = c("State", label),
+                            fill = list(color = "#E69F00"),
+                            font = list(size = 18, color = "black")),
+        cells = list(values = list(table_data$State,
+                                   table_data$disp_var),
+                     fill = list(color = plot_background_color),
+                     font = list(size = 12, color = "fff"),
+                     size=60)) %>% 
+        layout( plot_bgcolor = plot_background_color,
+                paper_bgcolor=plot_background_color)
+      
+    } else { # county level
+      # filtering and sorting table data
+      table_data = food_disparity_county_level %>%
+        arrange(desc(!!var_sym)) %>% 
+        mutate(disp_var = comma(!!var_sym)) %>% # adding commas to big nums
+        select(State, disp_var) %>%
+        head(10)
+      
+      # Create plotly table
+      plot_ly(type = 'table',
+              header = list(values = c("State", label),
+                            fill = list(color = "peach"),
+                            font = list(size = 18, color = "black")),
+              cells = list(values = list(table_data$State,
+                                         table_data$disp_var),
+                           fill = list(color = background_color),
+                           font = list(size = 12, color = "#FFF"),
+                           size=60)) %>% 
+        layout(plot_bgcolor = plot_background_color,
+               paper_bgcolor=plot_background_color
+        )
+      
+    }
+  })
+    
   
   #Creating the interactive histogram
   
@@ -311,7 +432,7 @@ server <- function(input, output) {
     plot_data <- state_level %>%
       filter(sex == input$gender & year_id == input$year & age_group_name == input$age)
     
-    #Creating histogram using ggplot and plotly. 
+    # Creating histogram using ggplot and plotly. 
     hp <- ggplot(plot_data, aes(x = mean_prev)) +
       geom_histogram(bins = 30, fill = "skyblue", color = "black", alpha = 0.7) +
       labs(
